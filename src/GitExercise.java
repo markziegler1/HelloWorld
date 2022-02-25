1b7c7433bbc86109ac23b3336b103658450fd80b
markziegler1 <mark893b@stud.kea.dk>
2022-02-25 09:30:04 +0100

    Added greet

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -1,6 +1,17 @@
-public class GitExercise {
-  public static void main(String[] args) {
-    System.out.println("It works!");
+import java.util.Scanner;
 
+public class GitExercise {
+
+void go(){
+  System.out.println("It works!");
+  System.out.println("Please enter your name");
+  Scanner in = new Scanner(System.in);
+  String name = in.nextLine();
+  System.out.println("Hej " + name);
+}
+
+  public static void main(String[] args) {
+    GitExercise go = new GitExercise();
+    go.go();
   }
 }
