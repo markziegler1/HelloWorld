61dc492850e4834eaab6886ea66a4a66e46ff5a3
markziegler1 <mark893b@stud.kea.dk>
2022-02-25 11:12:42 +0100

    This is stupid2

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -7,28 +7,28 @@ public class GitExercise {
 void go(){
   System.out.println("It works!");
   System.out.println("Please enter your name");
-  Scanner in = new Scanner(System.in);
   String name = in.nextLine();
   System.out.println("Hej " + name);
 }
 
-/*void askAboutAge(){
+void askAboutAge(){
   System.out.println("Please enter your age");
   int age = in.nextInt();
   System.out.println("Din alder er "+ age);
 }
 
- */
+
 
   void stupidMethod(){
     System.out.println("This is stupid");
+    System.out.println("hej");
   }
 
   public static void main(String[] args) {
     GitExercise go = new GitExercise();
     go.go();
-    //GitExercise askAboutAge = new GitExercise();
-    //askAboutAge.askAboutAge();
+    GitExercise askAboutAge = new GitExercise();
+    askAboutAge.askAboutAge();
     GitExercise stupidMethod = new GitExercise();
     stupidMethod.stupidMethod();
 
