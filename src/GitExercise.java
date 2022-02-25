1aa3d27013489873179e539c0a0f496154d1f44c
markziegler1 <mark893b@stud.kea.dk>
2022-02-25 09:34:21 +0100

    Added askAboutAge

--- src/GitExercise.java
+++ src/GitExercise.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class GitExercise {
 
+  int age;
+  Scanner in = new Scanner(System.in);
 void go(){
   System.out.println("It works!");
   System.out.println("Please enter your name");
@@ -10,8 +12,25 @@ void go(){
   System.out.println("Hej " + name);
 }
 
+/*void askAboutAge(){
+  System.out.println("Please enter your age");
+  int age = in.nextInt();
+  System.out.println("Din alder er "+ age);
+}
+
+ */
+
+  void stupidMethod(){
+    System.out.println("This is stupid");
+  }
+
   public static void main(String[] args) {
     GitExercise go = new GitExercise();
     go.go();
+    //GitExercise askAboutAge = new GitExercise();
+    //askAboutAge.askAboutAge();
+    GitExercise stupidMethod = new GitExercise();
+    stupidMethod.stupidMethod();
+
   }
 }
